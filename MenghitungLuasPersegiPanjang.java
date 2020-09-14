cfb028e03ca424b6380f30236ad0615dcfbfdbae
ayudian12 <70583611+ayudian12@users.noreply.github.com>
2020-09-14 14:28:13 +0700

    Add files via upload

---
+++ MenghitungLuasPersegiPanjang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menghitungluaspersegipanjang;
+
+/**
+ *
+ * @author ASUS A407M
+ */
+public class MenghitungLuasPersegiPanjang {
+    public static void main (String [] args ) {
+     double p=10, l=5, luas;
+     //*perhitungan
+     luas=p*l;
+     //*output
+     System.out.println("===PROGRAM MENGHITUNG LUAS PERSEGI PANJANG===");
+     System.out.println("Panjang\t= "+p+"cm");
+     System.out.println("Lebar\t= "+l+"cm");
+     System.out.println("Luas\t= PxL" );
+     System.out.println("\t= "+luas+"cm2");
+
+    
+    }
+    
+}
